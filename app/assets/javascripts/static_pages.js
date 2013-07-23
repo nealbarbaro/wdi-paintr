542aa3f29e04f4a2ff59fdd654a620274c0da47e
Neal Barbaro <nealbarbaro@gmail.com>
2013-07-23 15:19:22 -0700

    box divs

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,2 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+  var add_color = function() {
+    var box = $('<div>');
+    box.addClass('box');
+
+    var color = $('#color').val();
+    box.css('background-color', color);
+
+    $('#colors').prepend(box);
+    $('#color').val('').focus();
+  };
+
+
+  $('#add_color').on('click', add_color);
+})
